d2055e61e93095c682645dc79b55f872bd7f98bb
Carlos Andahur <carlos.andahur@gmail.com>
2017-05-11 18:13:36 -0300

    Progreso Curso Parte 5: Two way data binding y ng model

--- Controlador/controller.js
+++ Controlador/controller.js
@@ -1,5 +1,20 @@
 var app = angular.module("MyFirstApp",[]);
 app.controller("FirstController",function($scope){
 	$scope.nombre = "Carlos";
+	$scope.nuevoComentario = {};
+	$scope.comentarios = [
+		{
+			comentario: "Buen progreso",
+			username: "Carlos"
+		},
+		{
+			comentario: "Puedes mejorar",
+			username: "Sergio"
+		}
+	];
+	$scope.agregarComentario = function(){
+		$scope.comentarios.push($scope.nuevoComentario);
+		$scope.nuevoComentario = {};
+	}
 });
 
